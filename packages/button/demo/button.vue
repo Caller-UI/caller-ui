b4385d33e2b18039ec6f0ab3ad218dcf28e1e178
Wscthhh <wscthhh@163.com>
2022-08-07 15:40:17 +0800

    [代码重构]: 新增commitizen

---
+++ packages/button/demo/button.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+<template>
+<div>
+    <c-button type="success">success</c-button> 
+    <c-button type="primary">primary</c-button> 
+    <c-button type="info">info</c-button> 
+    <c-button type="warning">warning</c-button> 
+    <c-button type="danger">danger</c-button> 
+    <c-button>test</c-button> 
+</div>
+</template>
+<style lang="less" scoped>
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Cbutton from '../packages/button/demo/button.vue'
 </script>
 
 <template>
@@ -9,14 +10,8 @@
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-    <c-button type="success">success</c-button> 
-    <c-button type="primary">primary</c-button> 
-    <c-button type="info">info</c-button> 
-    <c-button type="warning">warning</c-button> 
-    <c-button type="danger">danger</c-button> 
-    <c-button>test</c-button> 
   </div>
-
+  <Cbutton />
 </template>
 
 <style lang="less" scoped>
