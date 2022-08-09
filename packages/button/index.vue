65d9a37132b8507407e4454695e2aacc973f1b20
Wscthhh <wscthhh@163.com>
2022-08-09 23:38:35 +0800

    [文档修改](eslint): eslint文件修改

--- packages/button/index.vue
+++ packages/button/index.vue
@@ -2,7 +2,6 @@
 import { computed } from 'vue'
 import { Types, Name, Sizes } from './const'
 import './Button.less'
-
 const props = defineProps({
   type: {
     type: String,
@@ -13,7 +12,6 @@ const props = defineProps({
     default: false
   }
 })
-console.log(props.round, props.type)
 
 const cClass = computed(() => {
   return [Name, Types.includes(props.type) ? `c-button-${props.type}` : 'c-button-default', props.round ? 'c-button-round' : '']
